636516de80ca6578732df76be1d3a46e74f8f338
Ulyanenkov Yaroslav <vashworld01@gmail.com>
2023-05-13 19:00:50 +0300

    Initial Commit

---
+++ SellableApp/SellableAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SellableAppApp.swift
+//  SellableApp
+//
+//  Created by Ярослав Ульяненков on 13.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct SellableAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
